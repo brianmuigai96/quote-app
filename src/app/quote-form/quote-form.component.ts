2714ed1c2a7baccbced3b63950e7f742c3c7955a
brianmuigai96 <brian.muigai@student.moringaschool.com>
2021-07-22 01:08:14 +0300

    adding the new quote function

--- src/app/quote-form/quote-form.component.ts
+++ src/app/quote-form/quote-form.component.ts
@@ -6,6 +6,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./quote-form.component.scss']
 })
 export class QuoteFormComponent implements OnInit {
+  newQuote = new Quote(0,"","","",new Date(),0,0);
+  @Output() add = new EventEmitter<Quote>();
+
+  addQuote(){
+this.add.emit(this.newQuote);
+this.newQuote = new Quote(0,"","","",new Date(),0,0);
+  }
 
   constructor() { }
 
