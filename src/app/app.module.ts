32e016c37534368eba110a78587794042888fcd5
brianmuigai96 <brian.muigai@student.moringaschool.com>
2021-07-22 00:10:13 +0300

    adding a qoute folder

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { QouteComponent } from './qoute/qoute.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    QouteComponent
   ],
   imports: [
     BrowserModule,
